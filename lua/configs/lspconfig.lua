4add5840667d3ac289f32923ff6b7b7bdf032529
findridoy <findridoy@gmail.com>
2024-05-26 15:59:50 +0600

    add lsp for php

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,9 +1,11 @@
--- EXAMPLE 
+-- EXAMPLE
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
+local util = require "lspconfig/util"
+
 local servers = { "html", "cssls" }
 
 -- lsps with default config
@@ -20,4 +22,12 @@ lspconfig.tsserver.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
+}
+
+lspconfig.intelephense.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  filetypes = { "php" },
+  root_dir = util.root_pattern "composer.json",
 }
